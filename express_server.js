51bb52d9e6c57b8a547154426cd90de1f7d64a7d
MrChan <mrkcwayne@gmail.com>
2023-06-26 16:11:26 -0600

    added a random 6 key generator and did changes to post for url so it reports the 6 generated string

--- express_server.js
+++ express_server.js
@@ -1,9 +1,23 @@
+/* eslint-env node */
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+// didnt use compass's example cause eslint keeps messing me up
+const generateRandomString = function() {
+  // Generate a random alphanumeric string of 6 characters
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,9 +34,11 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  const randomString = generateRandomString();
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.status(200).send(randomString); // Respond with the randomly generated string
 });
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
